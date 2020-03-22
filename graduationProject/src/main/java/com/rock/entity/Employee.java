5fae4ce3398bf87d910ceaed3f98b54567e34989
ChenQianRu <635579368@qq.com>
2020-03-22 16:28:35 +0800

    后端登录以及员工的增删改查

--- graduationProject/src/main/java/com/rock/entity/Employee.java
+++ graduationProject/src/main/java/com/rock/entity/Employee.java
@@ -1,5 +1,9 @@
 package com.rock.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,9 +13,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@TableName("Employee")
 public class Employee {
+    @TableId(value = "emp_id", type = IdType.AUTO)
     private String id;
+    @TableField(value = "emp_name")
     private String name;
+    @TableField(value = "emp_pwd")
     private String pwd;
+    @TableField(value = "emp_role")
     private String role;
 }
--- graduationProject/src/main/java/com/rock/mapper/EmployeeMapper.java
+++ graduationProject/src/main/java/com/rock/mapper/EmployeeMapper.java
@@ -1,9 +1,10 @@
 package com.rock.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.rock.entity.Employee;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeMapper {
-    Employee getEmpById (String empId);
+public interface EmployeeMapper extends BaseMapper<Employee> {
+   // Employee getEmpById (String empId);
 }
