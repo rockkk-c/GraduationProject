c15856186ce6ef21a941b3198ec9859f39cac708
rockkk-c <58465378+rockkk-c@users.noreply.github.com>
2020-03-22 17:31:48 +0800

    Delete EmployeeMapper.java

--- graduationProject/src/main/java/com/rock/mapper/EmployeeMapper.java
+++
@@ -1,9 +0,0 @@
-package com.rock.mapper;
-
-import com.rock.entity.Employee;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeMapper {
-    Employee getEmpById (String empId);
-}
