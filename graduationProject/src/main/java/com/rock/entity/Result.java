d25191a5514c273b239713845baa6684cf1aaede
ChenQianRu <635579368@qq.com>
2020-03-23 14:24:44 +0800

    Result纠错

--- graduationProject/src/main/java/com/rock/entity/Result.java
+++ graduationProject/src/main/java/com/rock/entity/Result.java
@@ -1,10 +1,14 @@
 package com.rock.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Result<T> {
 
     private Integer code;
