b7001320f3bea94a5619f3ecadc2d50c1babf1ca
ChenQianRu <635579368@qq.com>
2020-03-28 21:31:11 +0800

    Merge branch 'feature_java_qianru' of D:\毕业设设计\GraduationProject with conflicts.

--- d2-admin/src/main.js
+++ d2-admin/src/main.js
@@ -7,6 +7,8 @@ import d2Admin from '@/plugin/d2admin'
 // store
 import store from '@/store/index'
 
+Vue.prototype.$store = store
+
 // 菜单和路由设置
 import router from './router'
 import menuHeader from '@/menu/header'
@@ -19,7 +21,35 @@ Vue.use(d2Admin)
 import VCharts from 'v-charts'
 Vue.use(VCharts)
 
+
+// vue-apollo
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import VueApollo from 'vue-apollo'
+
+const httpLink = new HttpLink({
+  // You should use an absolute URL here
+  uri: 'http://localhost:8888/graphql'
+})
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+})
+
+// Install the vue plugin
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+})
+
+
 new Vue({
+  provide: apolloProvider.provide(),
   router,
   store,
   i18n,
