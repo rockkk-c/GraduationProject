560d1dec2633f91f8df2d50f78269c05f097fef0
ChenQianRu <635579368@qq.com>
2020-03-31 14:07:32 +0800

    风险预测页面

---
+++ d2-admin/src/views/system/risk-control/risk-predict/predict-result.vue
@@ -0,0 +1,43 @@
+<template>
+  <d2-container>
+    <h1>信息检测结果</h1>
+
+  </d2-container>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+export default {
+  methods: {
+
+  },
+  created () {
+  //  let applyId = this.$route.query.id
+    this.$apollo.query({
+      // Query
+
+      query: gql`query($applyId:String!){
+             resultDetails(applyId:$applyId)
+     }`,
+      variables: {
+        applyId: '100007'
+      }
+    }).then(res => {
+      this.overdueDetails = res.data.resultDetails.overdueDetails
+    }).catch(error => {
+      console.log(error)
+    })
+  },
+  data () {
+    return {
+      overdueDetails: [],
+
+
+
+    }
+  }
+}
+</script>
+
+<style>
+</style>
