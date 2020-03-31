04b5e6743a98e232f388526aefddb52fc688cdba
ChenQianRu <635579368@qq.com>
2020-03-31 14:29:05 +0800

    风险预测页面

--- d2-admin/src/views/system/risk-control/risk-predict/predict-result.vue
+++ d2-admin/src/views/system/risk-control/risk-predict/predict-result.vue
@@ -1,6 +1,7 @@
 <template>
   <d2-container>
     <h1>信息检测结果</h1>
+    <h3 >预测结果（1代表风险低，0代表风险高）：{{predictResult}}</h3>
 
   </d2-container>
 </template>
@@ -23,17 +24,28 @@ export default {
         applyId: '100007'
       }
     }).then(res => {
+      this.predictResult = res.data.resultDetails.predictResult
+      this.clientBF = res.data.resultDetails.clientBF
+      this.clientPhoneBF = res.data.resultDetails.clientPhoneBF
       this.overdueDetails = res.data.resultDetails.overdueDetails
+      this.OneDimenRelationshipBFDetails = res.data.resultDetails.OneDimenRelationshipBFDetails
+      this.OneDimenRelationshipPhoneBFDetails = res.data.resultDetails.OneDimenRelationshipPhoneBFDetails
+      this.TwoDimenRelationshipBFDetails = res.data.resultDetails.TwoDimenRelationshipBFDetails
+      this.TwoDimenRelationshipPhoneBFDetals = res.data.resultDetails.TwoDimenRelationshipPhoneBFDetals
     }).catch(error => {
       console.log(error)
     })
   },
   data () {
     return {
+      predictResult: '',
+      clientBF: '',
+      clientPhoneBF: '',
       overdueDetails: [],
-
-
-
+      OneDimenRelationshipBFDetails: [],
+      OneDimenRelationshipPhoneBFDetails: [],
+      TwoDimenRelationshipBFDetails: [],
+      TwoDimenRelationshipPhoneBFDetals: []
     }
   }
 }
